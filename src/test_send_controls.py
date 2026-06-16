c49ed8355db280f1ae4ca71494b105502bdac71b
Thomas Marks <marksta@umich.edu>
2026-06-16 10:37:26 -0400

    add control sending test

---
+++ src/test_send_controls.py
@@ -0,0 +1,63 @@
+import argparse
+import logging
+from pathlib import Path
+
+
+import lib.controls as controls
+from lib.labview import LabViewClient
+
+logger = logging.getLogger(__name__)
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=Path, help="The path to the command file to monitor")
+parser.add_argument(
+    "--cal-file", "-c", type=Path, help="The path to the thruster calibration file"
+)
+parser.add_argument(
+    "--sleep-interval",
+    type=float,
+    default=0.25,
+    help="How often, in seconds, to check for modifications to the command file",
+)
+parser.add_argument(
+    "--data-file",
+    "-d",
+    type=Path,
+    help="The file to which we write data received from LabVIEW. No data will be taken if this is empty",
+)
+parser.add_argument(
+    "--dwell-time",
+    "-t",
+    type=int,
+    default=5,
+    help="How long (in seconds) to dwell at each operating point before collecting data.",
+)
+
+voltages = [1.0, 2.0, 3.0, 4.0, 5.0]
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    control_file = Path(args.file)
+    data_file = Path(args.data_file)
+
+    server = controls.ThrusterController(
+        args.cal_file,
+        propellant="Kr",
+        control_to_file=control_file,
+        data_from_file=data_file,
+    )
+
+    with LabViewClient(dummy=True) as labview_client:
+        for voltage in voltages:
+            setpoint = controls.ControlPoint(
+                anode_mass_flow_rate_kg_s=0.0,
+                magnetic_field_scale=0.0,
+                cathode_flow_fraction=0.0,
+                discharge_voltage_v=voltage,
+            )
+            print(f"Controlling to setpoint: {setpoint}")
+            server.control_to(setpoint, client=labview_client)
+            data = server.take_data(client=labview_client)
+            print(f"Got data: {data}")
+
